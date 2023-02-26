93cf2c5903ee6d55526bd68c306101d80c392791
gandthecat <31240528+gandthecat@users.noreply.github.com>
2023-02-27 09:03:27 +1000

    Update test scripts to perform predictable string matching

--- countdown-timer.cy.ts
+++ countdown-timer.cy.ts
@@ -1,13 +1,7 @@
 import { match } from "assert";
 
 describe('Countdown Timer', () => {
-  
-  
-  //it.skip('Insert test here');
-
-  //it.skip('Insert test here');
-
-  //it.skip('Insert test here');
+  //Confirm that each countdown timer field is displaying a text value
   it('Countdown values should be non-empty', () => {
     cy.visit('');
     
@@ -18,30 +12,31 @@ describe('Countdown Timer', () => {
     cy.get(':nth-child(5) > :nth-child(2)').invoke('text').should('not.be.null')
   })
 
+  //Confirm that each countdown timer field formats hour values correctly where applicable
   it('Check hour string values', () => {
     cy.visit('');
     
     cy.get('[data-testid="category-filter-4a2788f8-e825-4d36-9894-efd4baf1cfae"] > [data-testid="category-filter-checkbox"]').click()
     cy.get('[data-testid="category-filter-9daef0d7-bf3c-4f50-921d-8e818c60fe61"] > [data-testid="category-filter-checkbox"]').click()
 
-    cy.get(':nth-child(1) > :nth-child(3) > :nth-child(1) > :nth-child(2)').invoke('text').should('match', /^[0-9]h?$/)
-    cy.get(':nth-child(3) > :nth-child(2) > :nth-child(2)').invoke('text').should('match', /^[0-9]h?$/)
-    cy.get(':nth-child(3) > :nth-child(3) > :nth-child(2)').invoke('text').should('match', /^[0-9]h?$/)
-    cy.get(':nth-child(4) > :nth-child(2)').invoke('text').should('match', /^[0-9]h?$/)
-    cy.get(':nth-child(5) > :nth-child(2)').invoke('text').should('match', /^[0-9]h?$/)
+    cy.get(':nth-child(1) > :nth-child(3) > :nth-child(1) > :nth-child(2)').invoke('text').should('match', /(^[0-9]h?$)||(^[0-9]m [0-9][0-9]s?$)/)
+    cy.get(':nth-child(3) > :nth-child(2) > :nth-child(2)').invoke('text').should('match', /(^[0-9]h?$)||(^[0-9]m [0-9][0-9]s?$)/)
+    cy.get(':nth-child(3) > :nth-child(3) > :nth-child(2)').invoke('text').should('match', /(^[0-9]h?$)||(^[0-9]m [0-9][0-9]s?$)/)
+    cy.get(':nth-child(4) > :nth-child(2)').invoke('text').should('match', /(^[0-9]h?$)||(^[0-9]m [0-9][0-9]s?$)/)
+    cy.get(':nth-child(5) > :nth-child(2)').invoke('text').should('match', /(^[0-9]h?$)||(^[0-9]m [0-9][0-9]s?$)/)
 
     cy.get('[data-testid="category-filter-4a2788f8-e825-4d36-9894-efd4baf1cfae"] > [data-testid="category-filter-checkbox"]').click()
     cy.get('[data-testid="category-filter-9daef0d7-bf3c-4f50-921d-8e818c60fe61"] > [data-testid="category-filter-checkbox"]').click()
   })
 
+  //Confirm that each countdown timer field formats minute & second values correctly where applicable
   it('Check sec & minute string values', () => {
     cy.visit('');
 
-    cy.get(':nth-child(1) > :nth-child(3) > :nth-child(1) > :nth-child(2)').invoke('text').should('match', /(^[0-9]m?$)|(^-[0-9]m [0-9][0-9]s?$)|(^[0-9]m [0-9][0-9]s?$)||(^-[0-9]m [0-9]s?$)/)
-    cy.get(':nth-child(3) > :nth-child(2) > :nth-child(2)').invoke('text').should('match', /(^[0-9]m?$)|(^-[0-9]m [0-9][0-9]s?$)|(^[0-9]m [0-9][0-9]s?$)||(^-[0-9]m [0-9]s?$)/)
-    cy.get(':nth-child(3) > :nth-child(3) > :nth-child(2)').invoke('text').should('match', /(^[0-9]m?$)|(^-[0-9]m [0-9][0-9]s?$)|(^[0-9]m [0-9][0-9]s?$)||(^-[0-9]m [0-9]s?$)/)
-    cy.get(':nth-child(4) > :nth-child(2)').invoke('text').should('match', /(^[0-9]m?$)|(^-[0-9]m [0-9][0-9]s?$)|(^[0-9]m [0-9][0-9]s?$)||(^-[0-9]m [0-9]s?$)/)
-    cy.get(':nth-child(5) > :nth-child(2)').invoke('text').should('match', /(^[0-9]m?$)|(^-[0-9]m [0-9][0-9]s?$)|(^[0-9]m [0-9][0-9]s?$)||(^-[0-9]m [0-9]s?$)/)
+    cy.get(':nth-child(1) > :nth-child(3) > :nth-child(1) > :nth-child(2)').invoke('text').should('match', /(^[0-9]m?$)||(^-[0-9]m [0-9][0-9]s?$)||(^[0-9]m [0-9][0-9]s?$)||(^-[0-9]m [0-9]s?$)/)
+    cy.get(':nth-child(3) > :nth-child(2) > :nth-child(2)').invoke('text').should('match', /(^[0-9]m?$)||(^-[0-9]m [0-9][0-9]s?$)||(^[0-9]m [0-9][0-9]s?$)||(^-[0-9]m [0-9]s?$)/)
+    cy.get(':nth-child(3) > :nth-child(3) > :nth-child(2)').invoke('text').should('match', /(^[0-9]m?$)||(^-[0-9]m [0-9][0-9]s?$)||(^[0-9]m [0-9][0-9]s?$)||(^-[0-9]m [0-9]s?$)/)
+    cy.get(':nth-child(4) > :nth-child(2)').invoke('text').should('match', /(^[0-9]m?$)||(^-[0-9]m [0-9][0-9]s?$)||(^[0-9]m [0-9][0-9]s?$)||(^-[0-9]m [0-9]s?$)/)
+    cy.get(':nth-child(5) > :nth-child(2)').invoke('text').should('match', /(^[0-9]m?$)||(^-[0-9]m [0-9][0-9]s?$)||(^[0-9]m [0-9][0-9]s?$)||(^-[0-9]m [0-9]s?$)/)
   })
-
 });
--- filters.cy.ts
+++ filters.cy.ts
@@ -1,10 +1,10 @@
+/// <reference types="cypress" />
+
 describe('Category Filters', () => {
-  //it.skip('Insert test here');
-
-  //it.skip('Insert test here');
-
-  //it.skip('Insert test here');
-
+  //For each filtered type
+  //Confirm that where only Greyhound/thoroughbred/harness checkbox is checked, that the other checkboxes are off
+  //Confirm that each race number displays the correct formatting (R1,R2,etc)
+  //Confirm that the location values are in alpha characters
   it('Filter greyhound', () => {
     cy.visit('');
     
@@ -15,22 +15,20 @@ describe('Category Filters', () => {
     cy.get('[data-testid="category-filter-161d9be2-e909-4326-8c2c-35ed71fb460b"] > [data-testid="category-filter-checkbox"]').should('not.be.checked')
     cy.get('[data-testid="category-filter-9daef0d7-bf3c-4f50-921d-8e818c60fe61"] > [data-testid="category-filter-checkbox"]').should('be.checked')
 
-    const locations = ["Ballarat", "Dubbo", "Wentworth Park"]
+    cy.get(':nth-child(1) > .race-name > .race-number').invoke('text').should('match', /^R[0-9][0-9]?$/)
+    cy.get(':nth-child(1) > .race-name > p').invoke('text').should('match', /[a-zA-Z ]*$/)
 
-    cy.get(':nth-child(1) > .race-name > .race-number').invoke('text').should('match', /^R[0-9]?$/)
-    cy.get(':nth-child(1) > .race-name > p').invoke('text').should('be.oneOf', locations)
+    cy.get(':nth-child(2) > .race-name > .race-number').invoke('text').should('match', /^R[0-9][0-9]?$/)
+    cy.get(':nth-child(2) > .race-name > p').invoke('text').should('match', /[a-zA-Z ]*$/)
 
-    cy.get(':nth-child(2) > .race-name > .race-number').invoke('text').should('match', /^R[0-9]?$/)
-    cy.get(':nth-child(2) > .race-name > p').invoke('text').should('be.oneOf', locations)
+    cy.get(':nth-child(3) > .race-name > .race-number').invoke('text').should('match', /^R[0-9][0-9]?$/)
+    cy.get(':nth-child(3) > .race-name > p').invoke('text').should('match', /[a-zA-Z ]*$/)
 
-    cy.get(':nth-child(3) > .race-name > .race-number').invoke('text').should('match', /^R[0-9]?$/)
-    cy.get(':nth-child(3) > .race-name > p').invoke('text').should('be.oneOf', locations)
+    cy.get(':nth-child(4) > .race-name > .race-number').invoke('text').should('match', /^R[0-9][0-9]?$/)
+    cy.get(':nth-child(4) > .race-name > p').invoke('text').should('match', /[a-zA-Z ]*$/)
 
-    cy.get(':nth-child(4) > .race-name > .race-number').invoke('text').should('match', /^R[0-9]?$/)
-    cy.get(':nth-child(4) > .race-name > p').invoke('text').should('be.oneOf', locations)
-
-    cy.get(':nth-child(5) > .race-name > .race-number').invoke('text').should('match', /^R[0-9]?$/)
-    cy.get(':nth-child(5) > .race-name > p').invoke('text').should('be.oneOf', locations)
+    cy.get(':nth-child(5) > .race-name > .race-number').invoke('text').should('match', /^R[0-9][0-9]?$/)
+    cy.get(':nth-child(5) > .race-name > p').invoke('text').should('match', /[a-zA-Z ]*$/)
 
     cy.get('[data-testid="category-filter-4a2788f8-e825-4d36-9894-efd4baf1cfae"] > [data-testid="category-filter-checkbox"]').click()
     cy.get('[data-testid="category-filter-161d9be2-e909-4326-8c2c-35ed71fb460b"] > [data-testid="category-filter-checkbox"]').click()
@@ -43,10 +41,6 @@ describe('Category Filters', () => {
   it('Filter thoroughbred', () => {
     cy.visit('');
     
-    const locations = ["Doomben", "Otaki", "Newcastle", "Charles Town", "Sandown", "Matamata", 
-                      "Albury", "Delta Downs", "Morphettville", "Yarra Valley", "Randwick",
-                      "Tuncurry","Mackay"]
-
     cy.get('[data-testid="category-filter-9daef0d7-bf3c-4f50-921d-8e818c60fe61"] > [data-testid="category-filter-checkbox"]').click()
     cy.get('[data-testid="category-filter-161d9be2-e909-4326-8c2c-35ed71fb460b"] > [data-testid="category-filter-checkbox"]').click()
 
@@ -54,20 +48,20 @@ describe('Category Filters', () => {
     cy.get('[data-testid="category-filter-161d9be2-e909-4326-8c2c-35ed71fb460b"] > [data-testid="category-filter-checkbox"]').should('not.be.checked')
     cy.get('[data-testid="category-filter-9daef0d7-bf3c-4f50-921d-8e818c60fe61"] > [data-testid="category-filter-checkbox"]').should('not.be.checked')
 
-    cy.get(':nth-child(1) > .race-name > .race-number').invoke('text').should('match', /^R[0-9]?$/)
-    cy.get(':nth-child(1) > .race-name > p').invoke('text').should('be.oneOf', locations)
+    cy.get(':nth-child(1) > .race-name > .race-number').invoke('text').should('match', /^R[0-9][0-9]?$/)
+    cy.get(':nth-child(1) > .race-name > p').invoke('text').should('match', /[a-zA-Z ]*$/)
 
-    cy.get(':nth-child(2) > .race-name > .race-number').invoke('text').should('match', /^R[0-9]?$/)
-    cy.get(':nth-child(2) > .race-name > p').invoke('text').should('be.oneOf', locations)
+    cy.get(':nth-child(2) > .race-name > .race-number').invoke('text').should('match', /^R[0-9][0-9]?$/)
+    cy.get(':nth-child(2) > .race-name > p').invoke('text').should('match', /[a-zA-Z ]*$/)
 
-    cy.get(':nth-child(3) > .race-name > .race-number').invoke('text').should('match', /^R[0-9]?$/)
-    cy.get(':nth-child(3) > .race-name > p').invoke('text').should('be.oneOf', locations)
+    cy.get(':nth-child(3) > .race-name > .race-number').invoke('text').should('match', /^R[0-9][0-9]?$/)
+    cy.get(':nth-child(3) > .race-name > p').invoke('text').should('match', /[a-zA-Z ]*$/)
 
-    cy.get(':nth-child(4) > .race-name > .race-number').invoke('text').should('match', /^R[0-9]?$/)
-    cy.get(':nth-child(4) > .race-name > p').invoke('text').should('be.oneOf', locations)
+    cy.get(':nth-child(4) > .race-name > .race-number').invoke('text').should('match', /^R[0-9][0-9]?$/)
+    cy.get(':nth-child(4) > .race-name > p').invoke('text').should('match', /[a-zA-Z ]*$/)
 
-    cy.get(':nth-child(5) > .race-name > .race-number').invoke('text').should('match', /^R[0-9]?$/)
-    cy.get(':nth-child(5) > .race-name > p').invoke('text').should('be.oneOf', locations)
+    cy.get(':nth-child(5) > .race-name > .race-number').invoke('text').should('match', /^R[0-9][0-9]?$/)
+    cy.get(':nth-child(5) > .race-name > p').invoke('text').should('match', /[a-zA-Z ]*$/)
 
     cy.get('[data-testid="category-filter-9daef0d7-bf3c-4f50-921d-8e818c60fe61"] > [data-testid="category-filter-checkbox"]').click()
     cy.get('[data-testid="category-filter-161d9be2-e909-4326-8c2c-35ed71fb460b"] > [data-testid="category-filter-checkbox"]').click()
@@ -80,8 +74,6 @@ describe('Category Filters', () => {
   it('Filter harness', () => {
     cy.visit('');
 
-    const locations = ["Menangle", "Melton", "Redcliffe"]
-
     cy.get('[data-testid="category-filter-4a2788f8-e825-4d36-9894-efd4baf1cfae"] > [data-testid="category-filter-checkbox"]').click()
     cy.get('[data-testid="category-filter-9daef0d7-bf3c-4f50-921d-8e818c60fe61"] > [data-testid="category-filter-checkbox"]').click()
 
@@ -89,20 +81,20 @@ describe('Category Filters', () => {
     cy.get('[data-testid="category-filter-161d9be2-e909-4326-8c2c-35ed71fb460b"] > [data-testid="category-filter-checkbox"]').should('be.checked')
     cy.get('[data-testid="category-filter-9daef0d7-bf3c-4f50-921d-8e818c60fe61"] > [data-testid="category-filter-checkbox"]').should('not.be.checked')
 
-    cy.get(':nth-child(1) > .race-name > .race-number').invoke('text').should('match', /^R[0-9]?$/)
-    cy.get(':nth-child(1) > .race-name > p').invoke('text').should('be.oneOf', locations)
+    cy.get(':nth-child(1) > .race-name > .race-number').invoke('text').should('match', /^R[0-9][0-9]?$/)
+    cy.get(':nth-child(1) > .race-name > p').invoke('text').should('match', /[a-zA-Z ]*$/)
 
-    cy.get(':nth-child(2) > .race-name > .race-number').invoke('text').should('match', /^R[0-9]?$/)
-    cy.get(':nth-child(2) > .race-name > p').invoke('text').should('be.oneOf', locations)
+    cy.get(':nth-child(2) > .race-name > .race-number').invoke('text').should('match', /^R[0-9][0-9]?$/)
+    cy.get(':nth-child(2) > .race-name > p').invoke('text').should('match', /[a-zA-Z ]*$/)
 
-    cy.get(':nth-child(3) > .race-name > .race-number').invoke('text').should('match', /^R[0-9]?$/)
-    cy.get(':nth-child(3) > .race-name > p').invoke('text').should('be.oneOf', locations)
+    cy.get(':nth-child(3) > .race-name > .race-number').invoke('text').should('match', /^R[0-9][0-9]?$/)
+    cy.get(':nth-child(3) > .race-name > p').invoke('text').should('match', /[a-zA-Z ]*$/)
 
-    cy.get(':nth-child(4) > .race-name > .race-number').invoke('text').should('match', /^R[0-9]?$/)
-    cy.get(':nth-child(4) > .race-name > p').invoke('text').should('be.oneOf', locations)
+    cy.get(':nth-child(4) > .race-name > .race-number').invoke('text').should('match', /^R[0-9][0-9]?$/)
+    cy.get(':nth-child(4) > .race-name > p').invoke('text').should('match', /[a-zA-Z ]*$/)
 
-    cy.get(':nth-child(5) > .race-name > .race-number').invoke('text').should('match', /^R[0-9]?$/)
-    cy.get(':nth-child(5) > .race-name > p').invoke('text').should('be.oneOf', locations)
+    cy.get(':nth-child(5) > .race-name > .race-number').invoke('text').should('match', /^R[0-9][0-9]?$/)
+    cy.get(':nth-child(5) > .race-name > p').invoke('text').should('match', /[a-zA-Z ]*$/)
 
     cy.get('[data-testid="category-filter-4a2788f8-e825-4d36-9894-efd4baf1cfae"] > [data-testid="category-filter-checkbox"]').click()
     cy.get('[data-testid="category-filter-9daef0d7-bf3c-4f50-921d-8e818c60fe61"] > [data-testid="category-filter-checkbox"]').click()
